3650dd54be91a08070978d1b24c1670db48f5cc4
Ayush Dwivedi <ayushd785@gmail.com>
2026-03-21 03:35:56 +0530

    added more controllers

--- src/controllers/todoController.ts
+++ src/controllers/todoController.ts
@@ -22,7 +22,52 @@ export const createTodo = async(req:AuthRequest, res: Response)=>{
 
 
     }catch (err: any) {
-    console.error("CREATE TODO ERROR:", err); // 🔥 MUST
+    console.error("CREATE TODO ERROR:", err);
     res.status(500).json({ error: err.message });
+    }
 }
+
+
+export const getTodos = async(req:AuthRequest, res: Response) =>{
+    try{
+        const userId = req.userId;
+        if(!userId){
+            return res.status(301).json({
+                msg:"User not found"
+            })
+        }
+        const todos = await todoService.getTodosByUser(userId);
+        return res.status(200).json(todos);
+    }catch(err:any){
+        res.status(500).json({ error: 'Failed to fetch todos' });
+    }
+}
+
+
+export const updateTodo = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { isCompleted } = req.body;
+    const updatedTodo = await todoService.toggleTodoCompletion(Number(id), isCompleted);
+    res.status(200).json(updatedTodo);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to update todo' });
+  }
+};
+
+
+export const deleteTodo = async(req:AuthRequest, res: Response)=>{
+    try{
+        const {id} = req.params;
+        if(!id){
+            return res.status(300).json({
+                msg:"Please provide the id of todo to delete"
+            })
+        }
+
+        await todoService.deleteTodo(Number(id));
+        res.status(204).send();
+    }catch (error) {
+    res.status(500).json({ error: 'Failed to delete todo' });
+  }
 }
